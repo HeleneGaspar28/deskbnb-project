b84afd43803bdacfe360f1ba8fc70dbb6cf333b3
Helene Gaspar <helene.gaspar@velopass.com>
2025-06-26 18:19:10 +0200

    bookings CRUD done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "pages#home"
 
   resources :desks, only: [:new, :create]
+  resources :bookings, only: [:create, :index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
