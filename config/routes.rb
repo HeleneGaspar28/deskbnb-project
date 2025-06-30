975cee9808cce49e636f1134d72bd0db65c48b4b
Helene Gaspar <helene.gaspar@velopass.com>
2025-06-30 14:44:10 +0200

    created mydesks page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   root to: "desks#index"
 
   resources :desks, only: [:new, :create, :update, :index, :show] do
+    collection do
+      get :mydesks
+    end
     resources :reviews, only: [:new, :create]
     resources :bookings, only: [:create]
   end
