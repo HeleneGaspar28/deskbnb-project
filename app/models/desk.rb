96cbdf7bd102ab6f09a21c9b30830aa9cbdbeb7c
Helene Gaspar <helene.gaspar@velopass.com>
2025-07-04 21:44:08 +0200

    added search bar in desks index

--- app/models/desk.rb
+++ app/models/desk.rb
@@ -1,4 +1,5 @@
 class Desk < ApplicationRecord
+  include PgSearch::Model
   belongs_to :user
   has_many :bookings
   has_many :reviews, dependent: :destroy
@@ -8,4 +9,12 @@ class Desk < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 
   validates :title, :description, :price_per_hour, :address, presence: true
+
+  #pg search
+  include PgSearch::Model
+  pg_search_scope :search_by_title_description_and_address,
+    against: [ :title, :description, :address ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
